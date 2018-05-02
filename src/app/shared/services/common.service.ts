2fd5b64741dd1fa0f697a7ac46d9d0f9c3c337ca
Yashwardhan Pauranik <yash@appointy.com>
2018-05-02 19:47:40 +0530

    mixcontent fix

--- src/app/shared/services/common.service.ts
+++ src/app/shared/services/common.service.ts
@@ -5,7 +5,7 @@ import {HttpClient} from '@angular/common/http';
 export class CommonService {
 
   private accessToken = 'access_token=dd0df21c8af5d929dff19f74506c4a8153d7acd34306b9761fd4a57cfa1d483c';
-  private apiPath = 'https://aiesec-task.herokuapp.com/http://gisapi-web-staging-1636833739.eu-west-1.elb.amazonaws.com/v2';
+  private apiPath = 'http://gisapi-web-staging-1636833739.eu-west-1.elb.amazonaws.com/v2';
 
   constructor(private http: HttpClient) { }
 
